243ff40561281ae760dc62992edfcc22eebfd402
Jonathan Carmack <jonathan.carmack@gsa.gov>
2017-11-14 17:33:26 -0500

    Added sms response

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+from twilio.twiml.messaging_response import MessagingResponse
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/smsResponse")
-def hanlde_response():
-    return "Hello World!"
+@app.route("/smsResponse", methods=['GET', 'POST'])
+def handle_response():
+	resp = MessagingResponse()
+	resp.message("Hello Jon Carmack")
+	return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
