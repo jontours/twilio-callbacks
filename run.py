186d78b6ae85119399b7543be53935cf66e2192c
Jonathan Carmack <jonathan.carmack@gsa.gov>
2017-11-14 10:39:36 -0500

    Added generic route

--- run.py
+++ run.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/smsResponse")
+def hanlde_response():
+    return "Hello World!"
+
 if __name__ == "__main__":
     app.run(debug=True)
